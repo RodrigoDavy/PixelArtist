a845a47476402095cfc646b3f7679d7e81f93d1b
RodrigoDavy <rodrigodavy@gmail.com>
2018-01-02 21:50:15 -0200

    Hides Action Bar for getting more screen space

--- app/src/main/java/rodrigodavy/com/github/pixelartist/MainActivity.java
+++ app/src/main/java/rodrigodavy/com/github/pixelartist/MainActivity.java
@@ -1,18 +1,15 @@
 package rodrigodavy.com.github.pixelartist;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -32,6 +29,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Hides ActionBar to free some precious screen space
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
 
         initPalette();
         initPixels();
