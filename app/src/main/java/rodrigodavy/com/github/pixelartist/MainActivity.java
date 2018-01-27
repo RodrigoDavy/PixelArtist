5b1c98cf4d5e32cac5b3ea7123fe0db44e8d685b
RodrigoDavy <rodrigodavy@gmail.com>
2018-01-27 15:11:26 -0200

    Added "open" menu action

--- app/src/main/java/rodrigodavy/com/github/pixelartist/MainActivity.java
+++ app/src/main/java/rodrigodavy/com/github/pixelartist/MainActivity.java
@@ -21,7 +21,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
