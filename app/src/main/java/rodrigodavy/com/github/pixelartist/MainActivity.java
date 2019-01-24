7786b6c727f87b0f0b361825e976c34b13d26ce3
RikilG <rikil.gajarla@gmail.com>
2019-01-24 17:44:29 +0530

    added prompt for storage permission

--- app/src/main/java/rodrigodavy/com/github/pixelartist/MainActivity.java
+++ app/src/main/java/rodrigodavy/com/github/pixelartist/MainActivity.java
@@ -1,5 +1,6 @@
 package rodrigodavy.com.github.pixelartist;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -40,6 +41,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -57,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String SETTINGS_GRID = "grid";
     private static final String URL_ABOUT = "https://github.com/RodrigoDavy/PixelArtist/blob/master/README.md";
+    private static final int MY_REQUEST_WRITE_STORAGE = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -402,8 +405,12 @@ public class MainActivity extends AppCompatActivity {
         if (!checkWriteExternalPermission()) {
             Toast toast = Toast.makeText(this, R.string.no_write_permission, Toast.LENGTH_LONG);
             toast.show();
+            //Request permission from user.
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    MY_REQUEST_WRITE_STORAGE);
 
-            return;
+            //return;
         }
 
         Bitmap bitmap = Bitmap.createBitmap(view.getWidth(),
@@ -470,6 +477,23 @@ public class MainActivity extends AppCompatActivity {
         String permission = android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
         int res = this.checkCallingPermission(permission);
         return (res == PackageManager.PERMISSION_GRANTED);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_REQUEST_WRITE_STORAGE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted
+                    // can run additional stuff here
+                } else {
+                    // permission denied
+                    Toast.makeText(this, R.string.no_write_permission, Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
     }
 
     private void updateDrawerHeader() {
